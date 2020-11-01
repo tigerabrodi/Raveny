e525253e944f6fd83ed0536a9b0f1e70496e84e7
Tiger Abrodi <tigerabrodi@gmail.com>
2020-11-01 09:57:05 +0100

    fix(eslint): fix linting errors

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,12 +1,18 @@
-import React from "react";
-import { ThemeProvider } from "styled-components";
-import { BrowserRouter as Router } from "react-router-dom";
-import { GlobalStyle } from "theme/globalStyles";
-import { theme } from "theme/theme";
+import React, { FunctionComponent, ReactElement, ReactNode } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { GlobalStyle } from 'theme/globalStyles'
+import { theme } from 'theme/theme'
 
-export const AppProviders: React.FC = ({ children }) => (
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <Router>{children}</Router>
-  </ThemeProvider>
-);
+type AppProviderProps = {
+    children: ReactNode
+}
+
+export const AppProviders: FunctionComponent<AppProviderProps> = ({
+    children,
+}: AppProviderProps): ReactElement => (
+    <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Router>{children}</Router>
+    </ThemeProvider>
+)
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
+import React, { FunctionComponent, ReactNode } from 'react'
 
-export const Home = () => {
-  return <div>Hello</div>;
-};
+export const Home: FunctionComponent<ReactNode> = () => {
+    return <div>Hello</div>
+}
