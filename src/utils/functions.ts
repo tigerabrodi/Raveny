7532f9ed60d76807553798149c06009d82406589
Tiger Abrodi <tigerabrodi@gmail.com>
2020-12-02 16:42:48 +0100

    feat(getRandomLabel): add generic utility function

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -2,15 +2,10 @@ import faker from 'faker'
 import { DietLabel, HealthLabel } from 'types'
 
 /**
- * Returns a random health label
- * @return {HealthLabel}
+ * Returns either a random Health or Diet Label
+ * @param {HealthLabel | DietLabel}
+ * @return {string}
  */
-export const getRandomHealthLabel = (): HealthLabel =>
-  faker.random.arrayElement(Object.values(HealthLabel)) as HealthLabel
-
-/**
- * Returns a random diet label
- * @return {DietLabel}
- */
-export const getRandomDietLabel = (): DietLabel =>
-  faker.random.arrayElement(Object.values(DietLabel)) as DietLabel
+export const getRandomLabel = <T extends typeof DietLabel | typeof HealthLabel>(
+  enumLabel: T
+): T[keyof T] => faker.random.arrayElement(Object.values(enumLabel))
