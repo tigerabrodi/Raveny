0de72f098d5c183f44d481dd9055c40b8693f2dd
Tiger Abrodi <49603590+tigerabrodi@users.noreply.github.com>
2020-12-20 13:21:57 +0100

    feat(footer): accessibility improvements (#40)

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import { HeartIcon, Text, TextLink, FooterWrapper } from './styles'
 export const Footer = () => (
   <FooterWrapper>
     <Text>
-      Built with <HeartIcon /> by{' '}
+      Built with <HeartIcon title="Heart" role="img" /> by{' '}
       <TextLink href="https://github.com/tigerabrodi" target="_blank">
         Tiger Abrodi
       </TextLink>
