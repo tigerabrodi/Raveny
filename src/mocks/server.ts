3d29087d96a4e05c4445817de1ce258c6caad6b9
tigerabrodi <tigerabrodi@gmail.com>
2020-12-27 09:28:40 +0100

    chore(msw): remove from storybook
    Now using setupServer, which will eventually be used for the tests.
    
    README.md has been updated.

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,4 +1,4 @@
-import { setupWorker } from 'msw'
+import { setupServer } from 'msw/node'
 import { handlers } from './handlers'
 // This configures a Service Worker with the given request handlers.
-export const worker = setupWorker(...handlers)
+export const worker = setupServer(...handlers)
