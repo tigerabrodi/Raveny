b6869b90401a0a5ebbb7be9ae27d3e216565f132
tigerabrodi <tigerabrodi@gmail.com>
2020-12-27 09:04:59 +0100

    fixup! fix failing lint

--- src/App.tsx
+++ src/App.tsx
@@ -16,21 +16,23 @@ const apiKEY = process.env.REACT_APP_API_KEY
 
 const App = () => (
   <AppProviders>
-    <Navigation />
-    {!apiURL && !apiKEY ? (
-      <NoEnvironmentVariables />
-    ) : (
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/search" component={Search} />
-        <Route exact path="/recipes" component={Recipes} />
-        <Route exact path="/recipes/vegan" component={Vegan} />
-        <Route exact path="/recipes/low-carb" component={LowCarb} />
-        <Route exact path="/recipes/high-protein" component={HighProtein} />
-        <Route exact path="*" component={NotFound} />
-      </Switch>
-    )}
-    <Footer />
+    <>
+      <Navigation />
+      {!apiURL && !apiKEY ? (
+        <NoEnvironmentVariables />
+      ) : (
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/recipes" component={Recipes} />
+          <Route exact path="/recipes/vegan" component={Vegan} />
+          <Route exact path="/recipes/low-carb" component={LowCarb} />
+          <Route exact path="/recipes/high-protein" component={HighProtein} />
+          <Route exact path="*" component={NotFound} />
+        </Switch>
+      )}
+      <Footer />
+    </>
   </AppProviders>
 )
 
