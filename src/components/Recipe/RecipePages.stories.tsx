a067a675912fcebe5b7d93a0c943de927ff32fb5
tigerabrodi <tigerabrodi@gmail.com>
2020-12-29 11:23:10 +0100

    feat(LoadMoreSpinner): Make existing Spinner Component generic
    Existing Spinner component can also be the load more spinner.
    
    Stories for recipe page with load more spinner has been created.

--- src/components/Recipe/RecipePages.stories.tsx
+++ src/components/Recipe/RecipePages.stories.tsx
@@ -1,5 +1,11 @@
 import { recipesData } from 'data'
-import { RecipesMain, RecipesSection, RecipesHeading } from './styles'
+import { Spinner } from 'components/Spinner/index'
+import {
+  RecipesMain,
+  RecipesSection,
+  RecipesHeading,
+  LoadMoreSpinnerSection,
+} from './styles'
 import { Recipe as RecipeComponent } from '.'
 
 export default {
@@ -15,5 +21,19 @@ export const RecipesPage = () => (
         <RecipeComponent recipe={recipe} key={recipe.uri} />
       ))}
     </RecipesSection>
+  </RecipesMain>
+)
+
+export const RecipesPageWithLoadMoreSpinner = () => (
+  <RecipesMain>
+    <RecipesHeading>456 Results</RecipesHeading>
+    <RecipesSection>
+      {recipesData.map((recipe) => (
+        <RecipeComponent recipe={recipe} key={recipe.uri} />
+      ))}
+    </RecipesSection>
+    <LoadMoreSpinnerSection>
+      <Spinner isLoadMoreSpinner />
+    </LoadMoreSpinnerSection>
   </RecipesMain>
 )
