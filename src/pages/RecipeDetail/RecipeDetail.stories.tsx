aa8a80363daa83f2e7fe3dfa8d1c0d34f74d3d81
tigerabrodi <tigerabrodi@gmail.com>
2021-01-16 16:09:24 +0100

    feat(RecipeDetail): add page adjusted to desktop

--- src/pages/RecipeDetail/RecipeDetail.stories.tsx
+++ src/pages/RecipeDetail/RecipeDetail.stories.tsx
@@ -1,5 +1,23 @@
 import { singleRecipeData } from 'mocks/data/single-recipe-data'
-import { RecipeMain } from './styles'
+import {
+  RecipeImage,
+  RecipeInfoSection,
+  RecipeMain,
+  RecipeName,
+  Servings,
+  TotalTime,
+  Calories,
+  RecipeImageSection,
+  IngredientsList,
+  TimeSection,
+  ServingsSection,
+  ServingsHeading,
+  TimeHeading,
+  IngredientItem,
+  IngredientImage,
+  IngredientText,
+  IngredientsHeading,
+} from './styles'
 import { RecipeDetail as RecipeComponent } from '.'
 
 export default {
@@ -9,4 +27,32 @@ export default {
 
 const [recipe] = singleRecipeData
 
-export const RecipeDetailPage = () => <RecipeMain />
+export const RecipeDetailPage = () => (
+  <RecipeMain>
+    <RecipeName> {recipe.label} </RecipeName>
+    <RecipeInfoSection>
+      <TimeSection>
+        <TimeHeading>Cooking Time</TimeHeading>
+        <TotalTime>{Math.round(recipe.totalTime)} mins</TotalTime>
+      </TimeSection>
+
+      <ServingsSection>
+        <ServingsHeading>Servings</ServingsHeading>
+        <Servings>{recipe.yield}</Servings>
+      </ServingsSection>
+    </RecipeInfoSection>
+    <RecipeImageSection>
+      <RecipeImage src={recipe.image} alt={recipe.label} />
+      <Calories>{recipe.calories} Calories</Calories>
+    </RecipeImageSection>
+    <IngredientsList>
+      <IngredientsHeading>Ingredients</IngredientsHeading>
+      {recipe.ingredients.map(({ image, text }) => (
+        <IngredientItem>
+          <IngredientImage src={image} alt={text} />
+          <IngredientText>{text}</IngredientText>
+        </IngredientItem>
+      ))}
+    </IngredientsList>
+  </RecipeMain>
+)
