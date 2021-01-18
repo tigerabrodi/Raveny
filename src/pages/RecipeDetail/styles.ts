3884b6c223de90520537e3e86522ae14faeeee60
Tiger Abrodi <49603590+tigerabrodi@users.noreply.github.com>
2021-01-18 16:57:54 +0100

    feat(RecipeDetail): make responsive (#75)

--- src/pages/RecipeDetail/styles.ts
+++ src/pages/RecipeDetail/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import { wrapperStyles } from 'styles'
+import { media } from 'theme/media'
 
 export const RecipeMain = styled.main`
   ${wrapperStyles}
@@ -9,8 +10,13 @@ export const RecipeMain = styled.main`
     'info'
     'imageSection'
     'ingredients';
+  row-gap: 1rem;
   align-items: center;
   justify-items: center;
+  padding-bottom: 3rem;
+  ${media.phone} {
+    padding-bottom: 1rem;
+  }
 `
 
 export const RecipeName = styled.h1`
@@ -18,9 +24,16 @@ export const RecipeName = styled.h1`
   font-weight: 600;
   font-family: ${({ theme }) => theme.fonts.Lora};
   color: ${({ theme }) => theme.colors.Orange};
-  font-size: 6rem;
   grid-area: title;
   color: ${({ theme }) => theme.colors.Orange};
+  font-size: 2.5rem;
+  text-align: center;
+  ${media.phone} {
+    font-size: 5rem;
+  }
+  ${media.desktop} {
+    font-size: 6rem;
+  }
 `
 
 export const RecipeInfoSection = styled.section`
@@ -29,52 +42,88 @@ export const RecipeInfoSection = styled.section`
   grid-template-areas: 'time servings';
   justify-content: space-evenly;
   align-items: center;
-  height: 85%;
-  width: 80%;
-  border-radius: 0.2rem;
   border: 0.2rem solid ${({ theme }) => theme.colors.Brown};
   box-shadow: 0 0.2rem 0.5rem black;
+  width: 90%;
+  height: 6rem;
+  ${media.custom(360)} {
+    width: 80%;
+  }
+  ${media.custom(410)} {
+    width: 75%;
+  }
+  ${media.phone} {
+    width: 85%;
+    height: 8rem;
+  }
+  ${media.desktop} {
+    width: 60%;
+  }
+  ${media.custom(1600)} {
+    width: 40%;
+  }
 `
 
 export const TimeSection = styled.section`
   grid-area: time;
-  height: 100%;
-  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-evenly;
+  width: 100%;
+  height: 100%;
+  ${media.phone} {
+    height: 99%;
+    border-left: 0.2rem solid ${({ theme }) => theme.colors.Brown};
+    border-right: 0.2rem solid ${({ theme }) => theme.colors.Brown};
+    padding: 0 0.5rem;
+  }
 `
 
 export const ServingsSection = styled.section`
   grid-area: servings;
-  height: 100%;
-  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-evenly;
+  width: 100%;
+  height: 100%;
+  ${media.phone} {
+    height: 99%;
+    border-left: 0.2rem solid ${({ theme }) => theme.colors.Brown};
+    border-right: 0.2rem solid ${({ theme }) => theme.colors.Brown};
+    padding: 0 0.5rem;
+  }
 `
 
 export const TimeHeading = styled.h2`
   font-weight: 400;
-  font-size: 1.8rem;
   color: ${({ theme }) => theme.colors.White};
   font-family: ${({ theme }) => theme.fonts.Montserrat};
+  font-size: 1.5rem;
+  ${media.phone} {
+    font-size: 1.8rem;
+  }
 `
 
 export const ServingsHeading = styled.h2`
   font-weight: 400;
-  font-size: 1.8rem;
   color: ${({ theme }) => theme.colors.White};
   font-family: ${({ theme }) => theme.fonts.Montserrat};
+  font-size: 1.5rem;
+  ${media.phone} {
+    font-size: 1.8rem;
+  }
 `
 
 export const TotalTime = styled.h3`
   text-shadow: 0 0.2rem 0.5rem black;
   font-family: ${({ theme }) => theme.fonts.Lora};
-  font-size: 2.5rem;
   color: ${({ theme }) => theme.colors.Orange};
+  font-size: 2rem;
+  ${media.phone} {
+    font-size: 2.5rem;
+  }
 `
 
 export const Servings = styled.h3`
@@ -82,23 +131,33 @@ export const Servings = styled.h3`
   font-family: ${({ theme }) => theme.fonts.Lora};
   font-size: 2.5rem;
   color: ${({ theme }) => theme.colors.Orange};
+  font-size: 2rem;
+  ${media.phone} {
+    font-size: 2.5rem;
+  }
 `
 
 export const RecipeImageSection = styled.section`
-  height: 100%;
-  width: 80%;
   grid-area: imageSection;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-evenly;
+  height: 26rem;
+  width: 90%;
+  ${media.phone} {
+    height: 55rem;
+    width: 80%;
+  }
+  ${media.desktop} {
+    height: 60rem;
+  }
 `
 
 export const RecipeImage = styled.img`
-  height: 80%;
   box-shadow: 0 0.2rem 0.5rem black;
   border: 0.2rem solid ${({ theme }) => theme.colors.Brown};
-  border-radius: 0.2rem;
+  height: 80%;
 `
 
 export const Calories = styled.h1`
@@ -107,52 +166,83 @@ export const Calories = styled.h1`
   letter-spacing: 0.2rem;
   font-family: ${({ theme }) => theme.fonts.Lora};
   color: ${({ theme }) => theme.colors.Orange};
-  font-size: 4.5rem;
+  font-size: 2.5rem;
+  ${media.phone} {
+    font-size: 5.5rem;
+  }
 `
 
 export const IngredientsList = styled.ul`
   grid-area: ingredients;
   min-height: 100%;
-  width: 100%;
+  width: 90%;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-evenly;
   row-gap: 1rem;
-  padding-bottom: 1rem;
 `
 
 export const IngredientsHeading = styled.h2`
   color: ${({ theme }) => theme.colors.Orange};
-  font-size: 5rem;
   font-family: ${({ theme }) => theme.fonts.Montserrat};
   font-weight: 400;
   text-shadow: 0 0.2rem 0.5rem black;
   text-decoration: underline;
+  font-size: 4rem;
+  ${media.phone} {
+    font-size: 5rem;
+  }
 `
 
 export const IngredientItem = styled.li`
   display: grid;
-  grid-template-areas: 'image text';
   align-items: center;
-  justify-items: center;
   width: 100%;
-  height: 7rem;
+  justify-items: center;
+  grid-template-areas: 'text';
+  grid-template-columns: 1fr;
+  min-height: 4rem;
+  ${media.phone} {
+    min-height: 9rem;
+  }
+  ${media.desktop} {
+    justify-items: center;
+    width: 90%;
+    min-height: 10rem;
+    grid-template-areas: 'image text';
+    grid-template-columns: 50% 50%;
+  }
+  ${media.custom(1600)} {
+    width: 75%;
+  }
 `
 
 export const IngredientImage = styled.img`
-  grid-area: image;
-  height: 100%;
-  box-shadow: 0 0.2rem 0.5rem black;
-  border: 0.2rem solid ${({ theme }) => theme.colors.Brown};
-  border-radius: 0.2rem;
+  display: none;
+  ${media.desktop} {
+    display: block;
+    grid-area: image;
+    height: 100%;
+    box-shadow: 0 0.2rem 0.5rem black;
+    border: 0.2rem solid ${({ theme }) => theme.colors.Brown};
+  }
 `
 
 export const IngredientText = styled.h3`
   grid-area: text;
   font-family: ${({ theme }) => theme.fonts.Montserrat};
-  font-size: 2rem;
   color: ${({ theme }) => theme.colors.White};
   font-weight: 400;
-  width: 40ch;
+  text-align: center;
+  font-size: 2rem;
+  ${media.phone} {
+    font-size: 2.5rem;
+  }
+  ${media.tablet} {
+    text-align: left;
+  }
+  ${media.desktop} {
+    width: 30ch;
+  }
 `
