abe7861103e6ac3734b0909a513d56f3cdfd2885
tigerabrodi <tigerabrodi@gmail.com>
2021-01-23 11:46:51 +0100

    feat(NotFound): replace icons and update styles

--- src/pages/NotFound/styles.ts
+++ src/pages/NotFound/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { ReactComponent as NotFoundSVG404 } from 'assets/404.svg'
+import { ReactComponent as SadIcon } from 'assets/sad.svg'
 import { media } from 'theme/media'
 
 export const NotFoundMain = styled.main`
@@ -8,17 +8,18 @@ export const NotFoundMain = styled.main`
   position: relative;
 `
 
-export const NotFoundSVG = styled(NotFoundSVG404)`
-  height: 100%;
-  width: 95%;
+export const NotFoundSVG = styled(SadIcon)`
+  height: 70%;
+  width: 60%;
   filter: drop-shadow(0 0 0.3rem ${({ theme }) => theme.colors.Orange});
+  fill: ${({ theme }) => theme.colors.Orange};
   position: absolute;
   top: 35%;
   left: 50%;
   transform: translate(-50%, -50%);
   ${media.tablet} {
     top: 40%;
-    height: 80%;
+    height: 70%;
     width: 80%;
   }
 `
