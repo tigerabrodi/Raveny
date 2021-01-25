739fa19c6124fceb066c0f4e364961d87b7dd262
tigerabrodi <tigerabrodi@gmail.com>
2021-01-25 21:00:04 +0100

    feat(NoEnvVar): remove page

--- src/App.tsx
+++ src/App.tsx
@@ -6,33 +6,25 @@ import { Home } from 'pages/Home'
 import { Search } from 'pages/Search'
 import { Recipes } from 'pages/Recipes'
 import { NotFound } from 'pages/NotFound'
-import { NoEnvironmentVariables } from 'pages/NoEnvironmentVariables'
 import { Vegan } from 'pages/Vegan'
 import { HighProtein } from 'pages/HighProtein'
 import { LowCarb } from 'pages/LowCarb'
 import { RecipeDetail } from 'pages/RecipeDetail'
 
-const apiURL = process.env.REACT_APP_API_URL
-const apiKEY = process.env.REACT_APP_API_KEY
-
 const App = () => (
   <AppProviders>
     <>
       <Navigation />
-      {!apiURL && !apiKEY ? (
-        <NoEnvironmentVariables />
-      ) : (
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/search" component={Search} />
-          <Route exact path="/recipes" component={Recipes} />
-          <Route exact path="/recipes/vegan" component={Vegan} />
-          <Route exact path="/recipes/low-carb" component={LowCarb} />
-          <Route exact path="/recipes/high-protein" component={HighProtein} />
-          <Route exact path="/recipe/:recipeId" component={RecipeDetail} />
-          <Route exact path="*" component={NotFound} />
-        </Switch>
-      )}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/recipes" component={Recipes} />
+        <Route exact path="/recipes/vegan" component={Vegan} />
+        <Route exact path="/recipes/low-carb" component={LowCarb} />
+        <Route exact path="/recipes/high-protein" component={HighProtein} />
+        <Route exact path="/recipe/:recipeId" component={RecipeDetail} />
+        <Route exact path="*" component={NotFound} />
+      </Switch>
       <Footer />
     </>
   </AppProviders>
