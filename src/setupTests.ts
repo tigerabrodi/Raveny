47033689771610dcf8d313fa4e1e40fe4461f585
tigerabrodi <tigerabrodi@gmail.com>
2021-01-25 09:24:31 +0100

    feat(msw): add handler & mock responses for tests
    Setup for tests has also been configured.

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,9 @@
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom/extend-expect'
+import { configure } from '@testing-library/react'
+import { server } from 'mocks/server'
+
+configure({ defaultHidden: true })
+
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
+afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
