4ad861563b82ecfcfeb3b4e0ef26a3d8251482af
tigerabrodi <tigerabrodi@gmail.com>
2021-01-30 10:37:28 +0100

    test: create util assertions and refactor app.test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,7 @@
 import App from 'App'
 import {
+  ByRoleMatcher,
+  ByRoleOptions,
   render,
   screen,
   userEvent,
@@ -7,36 +9,41 @@ import {
   within,
 } from 'test/utils'
 
+const getByRoleInDocument = (
+  role: ByRoleMatcher,
+  { name, level }: { name?: string | RegExp; level?: number } = {}
+) => {
+  expect(screen.getByRole(role, { name, level })).toBeInTheDocument()
+}
+
+const getWithinElementRoleInDocument = (
+  element: HTMLElement,
+  role: ByRoleMatcher,
+  { name, level }: ByRoleOptions = {}
+) => {
+  expect(within(element).getByRole(role, { name, level })).toBeInTheDocument()
+}
+
 test('should allow simple user search flow', async () => {
   render(<App />)
-  expect(screen.getByRole('link', { name: /raveny/i })).toBeInTheDocument()
 
-  expect(screen.getByRole('link', { name: /vegan/i })).toBeInTheDocument()
+  getByRoleInDocument('link', { name: /raveny/i })
+  getByRoleInDocument('link', { name: /vegan/i })
+  getByRoleInDocument('link', { name: /high protein/i })
+  getByRoleInDocument('link', { name: /low carb/i })
 
-  expect(
-    screen.getByRole('link', { name: /high protein/i })
-  ).toBeInTheDocument()
+  getByRoleInDocument('heading', {
+    name: /Find Your Dream Recipes and Enjoy./i,
+    level: 1,
+  })
 
-  expect(screen.getByRole('link', { name: /low carb/i })).toBeInTheDocument()
-
-  expect(
-    screen.getByRole('heading', {
-      name: /Find Your Dream Recipes and Enjoy./i,
-      level: 1,
-    })
-  ).toBeInTheDocument()
-
-  expect(
-    screen.getByRole('link', { name: /tiger abrodi/i })
-  ).toBeInTheDocument()
+  getByRoleInDocument('link', { name: /tiger abrodi/i })
 
   userEvent.click(screen.getAllByRole('link', { name: /search/i })[1])
 
   expect(window.location.pathname).toBe('/search')
 
-  expect(
-    screen.getByRole('heading', { name: /Start Cooking Today!/i, level: 1 })
-  ).toBeInTheDocument()
+  getByRoleInDocument('heading', { name: /Start Cooking Today!/i, level: 1 })
 
   userEvent.type(screen.getByLabelText(/Search recipes/i), 'chicken')
 
@@ -46,101 +53,89 @@ test('should allow simple user search flow', async () => {
 
   expect(window.location.pathname).toBe('/recipes')
 
-  expect(
-    screen.getByRole('heading', { name: /120230 Results/i, level: 1 })
-  ).toBeInTheDocument()
+  getByRoleInDocument('heading', { name: /120230 Results/i, level: 1 })
 
   const firstRecipe = screen.getByRole('link', { name: /Chicken Vesuvio/i })
 
-  expect(
-    within(firstRecipe).getByRole('img', { name: /Chicken Vesuvio/i })
-  ).toBeInTheDocument()
+  getWithinElementRoleInDocument(firstRecipe, 'img', {
+    name: /Chicken Vesuvio/i,
+  })
 
-  expect(
-    within(firstRecipe).getByRole('heading', {
-      name: /Calories: 1057/i,
-      level: 2,
-    })
-  ).toBeInTheDocument()
+  getWithinElementRoleInDocument(firstRecipe, 'heading', {
+    name: /Calories: 1057/i,
+    level: 2,
+  })
 
-  expect(
-    within(firstRecipe).getByRole('heading', { name: /Servings: 4/i, level: 2 })
-  ).toBeInTheDocument()
+  getWithinElementRoleInDocument(firstRecipe, 'heading', {
+    name: /Servings: 4/i,
+    level: 2,
+  })
 
-  expect(
-    within(firstRecipe).getByRole('heading', { name: /sulfites/i, level: 3 })
-  ).toBeInTheDocument()
+  getWithinElementRoleInDocument(firstRecipe, 'heading', {
+    name: /sulfites/i,
+    level: 3,
+  })
 
-  expect(
-    within(firstRecipe).getByRole('heading', { name: /low-carb/i, level: 3 })
-  ).toBeInTheDocument()
+  getWithinElementRoleInDocument(firstRecipe, 'heading', {
+    name: /low-carb/i,
+    level: 3,
+  })
 
-  expect(
-    within(firstRecipe).getByRole('heading', { name: /peanut-free/i, level: 3 })
-  ).toBeInTheDocument()
+  getWithinElementRoleInDocument(firstRecipe, 'heading', {
+    name: /peanut-free/i,
+    level: 3,
+  })
 
-  expect(
-    within(firstRecipe).getByRole('heading', {
-      name: /Tree-Nut-Free/i,
-      level: 3,
-    })
-  ).toBeInTheDocument()
+  getWithinElementRoleInDocument(firstRecipe, 'heading', {
+    name: /Tree-Nut-Free/i,
+    level: 3,
+  })
 
   const secondRecipe = screen.getByRole('link', { name: /Chicken Paprikash/i })
 
-  expect(
-    within(secondRecipe).getByRole('img', { name: /Chicken Paprikash/i })
-  ).toBeInTheDocument()
+  getWithinElementRoleInDocument(secondRecipe, 'img', {
+    name: /Chicken Paprikash/i,
+  })
 
-  expect(
-    within(secondRecipe).getByRole('heading', {
-      name: /Calories: 1011/i,
-      level: 2,
-    })
-  ).toBeInTheDocument()
+  getWithinElementRoleInDocument(secondRecipe, 'heading', {
+    name: /Calories: 1011/i,
+    level: 2,
+  })
 
-  expect(
-    within(secondRecipe).getByRole('heading', {
-      name: /Servings: 3/i,
-      level: 2,
-    })
-  ).toBeInTheDocument()
+  getWithinElementRoleInDocument(secondRecipe, 'heading', {
+    name: /Servings: 3/i,
+    level: 2,
+  })
 
-  expect(
-    within(secondRecipe).getByRole('heading', { name: /sulfites/i, level: 3 })
-  ).toBeInTheDocument()
+  getWithinElementRoleInDocument(secondRecipe, 'heading', {
+    name: /sulfites/i,
+    level: 3,
+  })
 
-  expect(
-    within(secondRecipe).getByRole('heading', { name: /FODMAP/i, level: 3 })
-  ).toBeInTheDocument()
+  getWithinElementRoleInDocument(secondRecipe, 'heading', {
+    name: /FODMAP/i,
+    level: 3,
+  })
 
-  expect(
-    within(secondRecipe).getByRole('heading', {
-      name: /high-protein/i,
-      level: 3,
-    })
-  ).toBeInTheDocument()
+  getWithinElementRoleInDocument(secondRecipe, 'heading', {
+    name: /high-protein/i,
+    level: 3,
+  })
 
-  expect(
-    within(secondRecipe).getByRole('heading', {
-      name: /peanut-free/i,
-      level: 3,
-    })
-  ).toBeInTheDocument()
+  getWithinElementRoleInDocument(secondRecipe, 'heading', {
+    name: /peanut-free/i,
+    level: 3,
+  })
 
-  expect(
-    within(secondRecipe).getByRole('heading', {
-      name: /Tree-Nut-Free/i,
-      level: 3,
-    })
-  ).toBeInTheDocument()
+  getWithinElementRoleInDocument(secondRecipe, 'heading', {
+    name: /Tree-Nut-Free/i,
+    level: 3,
+  })
 
-  expect(
-    within(secondRecipe).getByRole('heading', {
-      name: /Alcohol-Free/i,
-      level: 3,
-    })
-  ).toBeInTheDocument()
+  getWithinElementRoleInDocument(secondRecipe, 'heading', {
+    name: /Alcohol-Free/i,
+    level: 3,
+  })
 
   userEvent.click(firstRecipe)
 
@@ -150,59 +145,37 @@ test('should allow simple user search flow', async () => {
     '/recipe/b79327d05b8e5b838ad6cfd9576b30b6'
   )
 
-  expect(
-    screen.getByRole('heading', { name: /Chicken Vesuvio/i, level: 1 })
-  ).toBeInTheDocument()
+  getByRoleInDocument('heading', { name: /Chicken Vesuvio/i, level: 1 })
 
-  expect(
-    screen.getByRole('heading', { name: /cooking Time 60 mins/i, level: 2 })
-  ).toBeInTheDocument()
+  getByRoleInDocument('heading', { name: /cooking Time 60 mins/i, level: 2 })
 
-  expect(
-    screen.getByRole('heading', { name: /Servings 4/i, level: 2 })
-  ).toBeInTheDocument()
+  getByRoleInDocument('heading', { name: /Servings 4/i, level: 2 })
 
-  expect(
-    screen.getByRole('img', { name: /Chicken Vesuvio/i })
-  ).toBeInTheDocument()
+  getByRoleInDocument('img', { name: /Chicken Vesuvio/i })
 
-  expect(
-    screen.getByRole('heading', { name: /1057 calories/i, level: 1 })
-  ).toBeInTheDocument()
+  getByRoleInDocument('heading', { name: /1057 calories/i, level: 1 })
 
-  expect(
-    screen.getByRole('heading', { name: /ingredients/i, level: 2 })
-  ).toBeInTheDocument()
+  getByRoleInDocument('heading', { name: /ingredients/i, level: 2 })
 
   const firstListItem = within(screen.getByRole('list')).getAllByRole(
     'listitem'
   )[0]
 
-  /*   expect(
-    within(firstListItem).getByRole('img', { name: 'Salt and pepper' })
-  ).toBeInTheDocument() */
+  getWithinElementRoleInDocument(firstListItem, 'img')
 
-  expect(
-    within(firstListItem).getByRole('heading', {
-      name: /salt and pepper/i,
-      level: 3,
-    })
-  ).toBeInTheDocument()
+  getWithinElementRoleInDocument(firstListItem, 'heading', {
+    name: /salt and pepper/i,
+    level: 3,
+  })
 
   const secondListItem = within(screen.getByRole('list')).getAllByRole(
     'listitem'
   )[1]
 
-  /*   expect(
-    within(secondListItem).getByRole('img', {
-      name: /1 cup frozen peas, thawed/i,
-    })
-  ).toBeInTheDocument() */
+  getWithinElementRoleInDocument(secondListItem, 'img')
 
-  expect(
-    within(secondListItem).getByRole('heading', {
-      name: /1 cup frozen peas, thawed/i,
-      level: 3,
-    })
-  ).toBeInTheDocument()
+  getWithinElementRoleInDocument(secondListItem, 'heading', {
+    name: /1 cup frozen peas, thawed/i,
+    level: 3,
+  })
 })
